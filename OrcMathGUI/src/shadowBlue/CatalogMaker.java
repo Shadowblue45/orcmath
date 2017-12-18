b281b3a311132a9b41564d456d5d0164572fa95e
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2017-12-18 11:44:00 -0500

    fixed edit

--- OrcMathGUI/src/shadowBlue/CatalogMaker.java
+++ OrcMathGUI/src/shadowBlue/CatalogMaker.java
@@ -7,7 +7,6 @@ public class CatalogMaker {
 	
 	private static ArrayList<VideoGames> catalog;
 	private static Scanner in = new Scanner(System.in);
-	private static String s1  = in.nextLine();
 	  
 	public CatalogMaker() {
 		catalog = new ArrayList<VideoGames>();
@@ -20,7 +19,7 @@ public class CatalogMaker {
 		//for(VideoGames t: catalog) {
 		//	System.out.println(t);
 		//}
-		getUserInput(s);
+		getUserInput();
 		System.out.println(getContents());
 	}
 
@@ -31,23 +30,22 @@ public class CatalogMaker {
 		}
 		return data;
 	}
-	public static void getUserInput(CatalogMaker s) {
-		String character = s.s1;
-		boolean age = false;
-		String s2 = "";
+	public static void getUserInput() {
+		System.out.println("What is the name of that video game character?");
+		String character = in.nextLine();
 		System.out.println("What is the age of that video game character?");
 		String ages = in.nextLine();
-		while(!age) {
-			for(int i = 0; i< ages.length()-1;i++) {
-				if(ages.substring(i, i+1).compareTo("0") >=0 && ages.substring(i, i+1).compareTo("9") <=0) {
-					s2 = s2 + ages.substring(i, i+1);
-				}
-			}
-			if(s2.length() > 1) {
-				int result = Integer.parseInt(s2);
-				catalog.add(new VideoGames(character,result));
-				age = true;
-			}
-		}
+		catalog.add(new VideoGames(character,Integer.parseInt(ages)));
+//		while(!ageInputed) {
+//			for(int i = 0; i< ages.length()-1;i++) {
+//				if(ages.substring(i, i+1).compareTo("0") >=0 && ages.substring(i, i+1).compareTo("9") <=0) {
+//					s2 = s2 + ages.substring(i, i+1);
+//				}
+//			}
+//			if(s2.length() > 1) {
+//				int result = Integer.parseInt(s2);
+//				age = true;
+//			}
+//		}
 	}
 }
