848b16e01a2673d22e9a4bbc9e0255a3ecfe1646
BT_1N3_32 <BT_1N3_32@BT_1N3_32.bktech.local>
2018-01-02 11:30:22 -0500

    custom button

---
+++ OrcMathGUI/src/shadowBlue/CustomButton.java
@@ -0,0 +1,44 @@
+package shadowBlue;
+
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class CustomButton extends Button {
+	
+	private String s1 = "It was";
+	private String s2 = "I Dio";
+	private Color blue;
+
+	public CustomButton(int x, int y) {
+		super(x, y, 20, 40, text, null);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+	}
+	
+	public void drawButton(Graphics2D g, boolean hover){
+		g.setColor(Color.BLUE);
+		g.drawString(s1, 10, 20);
+		g.drawString(s2, 20, 20);
+		g.fill3DRect(10, 10, 10, 20, hover);
+	}
+
+	void updateString1(String string){
+		
+	}
+
+	void updateString2(String string){
+		
+	}
+
+	void setIconColor(Color color) {
+		
+	}
+
+}
